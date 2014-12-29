304d424123ce5819c518ea1aa77834428b22fd9e
Robert Escuain <rescuain@opentrends.net>
2014-12-29 12:53:28 +0100

    updated some views

--- app/views/subscriptors/index.blade.php
+++ app/views/subscriptors/index.blade.php
@@ -2,26 +2,25 @@
 
 @section('content')
 
-<h1>All Subscriptors</h1>
+<h1>Subscriptors</h1>
 
-<p>{{ link_to_route('subscriptors.create', 'Add New Subscriptor', null, array('class' => 'btn btn-lg btn-success')) }}</p>
+<p>{{ link_to_route('subscriptors.create', 'Afegir nou', null, array('class' => 'btn btn-lg btn-success')) }}</p>
 
 @if ($subscriptors->count())
 	<table class="table table-striped">
 		<thead>
 			<tr>
-				<th>AdrecaDomicili</th>
-				<th>AdrecaEnviament</th>
-				<th>Cognom1</th>
-				<th>Cognom2</th>
-				<th>CompteCorrent</th>
-				<th>Contrasenya</th>
-				<th>DataNaixement</th>
-				<th>Dni</th>
 				<th>Email</th>
-				<th>EstaBloquejat</th>
 				<th>Nom</th>
-				<th>TelefonContacte</th>
+				<th>Cognom 1</th>
+				<th>Cognom 2</th>
+				<th>DNI</th>
+				<th>Data Naixement</th>
+				<th>Adreça Domicili</th>
+				<th>Adreça Enviament</th>
+				<th>Compte Corrent</th>
+				<th>Telèfon Contacte</th>
+				<th>Bloquejat</th>
 				<th>&nbsp;</th>
 			</tr>
 		</thead>
@@ -29,30 +28,29 @@
 		<tbody>
 			@foreach ($subscriptors as $subscriptor)
 				<tr>
-					<td>{{{ $subscriptor->adrecaDomicili }}}</td>
-					<td>{{{ $subscriptor->adrecaEnviament }}}</td>
+					<td>{{{ $subscriptor->email }}}</td>
+					<td>{{{ $subscriptor->nom }}}</td>
 					<td>{{{ $subscriptor->cognom1 }}}</td>
 					<td>{{{ $subscriptor->cognom2 }}}</td>
-					<td>{{{ $subscriptor->compteCorrent }}}</td>
-					<td>{{{ $subscriptor->contrasenya }}}</td>
-					<td>{{{ $subscriptor->dataNaixement }}}</td>
 					<td>{{{ $subscriptor->dni }}}</td>
-					<td>{{{ $subscriptor->email }}}</td>
-					<td>{{{ $subscriptor->estaBloquejat }}}</td>
-					<td>{{{ $subscriptor->nom }}}</td>
+					<td>{{{ $subscriptor->dataNaixement }}}</td>
+					<td>{{{ $subscriptor->adrecaDomicili }}}</td>
+					<td>{{{ $subscriptor->adrecaEnviament }}}</td>
+					<td>{{{ $subscriptor->compteCorrent }}}</td>
 					<td>{{{ $subscriptor->telefonContacte }}}</td>
+					<td>{{{ $subscriptor->estaBloquejat }}}</td>
                     <td>
                         {{ Form::open(array('style' => 'display: inline-block;', 'method' => 'DELETE', 'route' => array('subscriptors.destroy', $subscriptor->id))) }}
-                            {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
+                            {{ Form::submit('Esborra', array('class' => 'btn btn-danger')) }}
                         {{ Form::close() }}
-                        {{ link_to_route('subscriptors.edit', 'Edit', array($subscriptor->id), array('class' => 'btn btn-info')) }}
+                        {{ link_to_route('subscriptors.edit', 'Edita', array($subscriptor->id), array('class' => 'btn btn-info')) }}
                     </td>
 				</tr>
 			@endforeach
 		</tbody>
 	</table>
 @else
-	There are no subscriptors
+	No hi ha subscriptors
 @endif
 
 @stop
--- app/views/subscriptors/show.blade.php
+++ app/views/subscriptors/show.blade.php
@@ -9,35 +9,33 @@
 <table class="table table-striped">
 	<thead>
 		<tr>
-			<th>AdrecaDomicili</th>
-				<th>AdrecaEnviament</th>
-				<th>Cognom1</th>
-				<th>Cognom2</th>
-				<th>CompteCorrent</th>
-				<th>Contrasenya</th>
-				<th>DataNaixement</th>
-				<th>Dni</th>
 				<th>Email</th>
-				<th>EstaBloquejat</th>
 				<th>Nom</th>
-				<th>TelefonContacte</th>
+				<th>Cognom 1</th>
+				<th>Cognom 2</th>
+				<th>DNI</th>
+				<th>Data Naixement</th>
+				<th>Adreça Domicili</th>
+				<th>Adreça Enviament</th>
+				<th>Compte Corrent</th>
+				<th>Telèfon Contacte</th>
+				<th>Bloquejat</th>
 		</tr>
 	</thead>
 
 	<tbody>
 		<tr>
-			<td>{{{ $subscriptor->adrecaDomicili }}}</td>
-					<td>{{{ $subscriptor->adrecaEnviament }}}</td>
+					<td>{{{ $subscriptor->email }}}</td>
+					<td>{{{ $subscriptor->nom }}}</td>
 					<td>{{{ $subscriptor->cognom1 }}}</td>
 					<td>{{{ $subscriptor->cognom2 }}}</td>
-					<td>{{{ $subscriptor->compteCorrent }}}</td>
-					<td>{{{ $subscriptor->contrasenya }}}</td>
-					<td>{{{ $subscriptor->dataNaixement }}}</td>
 					<td>{{{ $subscriptor->dni }}}</td>
-					<td>{{{ $subscriptor->email }}}</td>
-					<td>{{{ $subscriptor->estaBloquejat }}}</td>
-					<td>{{{ $subscriptor->nom }}}</td>
+					<td>{{{ $subscriptor->dataNaixement }}}</td>
+					<td>{{{ $subscriptor->adrecaDomicili }}}</td>
+					<td>{{{ $subscriptor->adrecaEnviament }}}</td>
+					<td>{{{ $subscriptor->compteCorrent }}}</td>
 					<td>{{{ $subscriptor->telefonContacte }}}</td>
+					<td>{{{ $subscriptor->estaBloquejat }}}</td>
                     <td>
                         {{ Form::open(array('style' => 'display: inline-block;', 'method' => 'DELETE', 'route' => array('subscriptors.destroy', $subscriptor->id))) }}
                             {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
